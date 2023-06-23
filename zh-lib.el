06f2f6b648c62246da966376bafacfbbf5fc7a99
dalu <mou.tong@outlook.com>
2023-06-24 00:48:06 +0800

    Fix dependency problem

--- zh-lib.el
+++ zh-lib.el
@@ -3,7 +3,7 @@
 ;; Author: dalu <mou.tong@qq.com>
 ;; URL: https://github.com/dalugm/zh-lib.el
 ;; Version: 0.1.0
-;; Package-Requires: (emacs "24")
+;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: Chinese, library
 
 ;; This file is NOT part of GNU Emacs.
